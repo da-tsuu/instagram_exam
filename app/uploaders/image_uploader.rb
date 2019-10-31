ac4301e84fef36d54ca8ef0fb32ed96587b8db58
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-10-31 01:41:27 +0000

    例外処理を実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
 
-  process :resize_to_limit => [50, 50]
+  process :resize_to_limit => [300, 300]
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- app/uploaders/profileimage_uploader.rb
+++ app/uploaders/profileimage_uploader.rb
@@ -1,31 +1,10 @@
 class ProfileimageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process :resize_to_limit => [640, 480]
-  process :convert => 'jpg'
-  version :thumb do
-    process :resize_to_limit => [300, 300]
-  end
-  version :thumb100 do
-    process :resize_to_limit => [100, 100]
-  end
-  version :thumb30 do
-    process :resize_to_limit => [30, 30]
-  end
-
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
-
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
 
   storage :file
+
+  process :resize_to_limit => [100, 100]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
